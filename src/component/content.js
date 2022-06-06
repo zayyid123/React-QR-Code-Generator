8e40496e46547bd3f6df5c678b77352c150af239
zayyid123 <mochamadzayyid@gmail.com>
2022-06-06 16:13:17 +0700

    fiks fitur

--- src/component/content.js
+++ src/component/content.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import InputContent from './inputContent'
+import OutputContent from './outputContent'
 
 const Content = () => {
+    const [value, setValue] = React.useState('')
+
+    const handleChangeValue = (e) => {
+        setValue(e.target.value)
+    }
+
     return (
         <div className='bg-slate-200 text-slate-900 dark:bg-[#211E2D] dark:text-white'>
             <div className='max-w-5xl mx-auto min-h-screen'>
-                <div className='flex justify-around items-start pt-10 flex-wrap'>
-                    <h1>Heloo</h1>
+                <div className='flex justify-evenly items-start pt-10 flex-wrap'>
+                    <InputContent value={value} handleChangeValue={handleChangeValue} />
+                    <OutputContent value={value} />
                 </div>
             </div>
         </div>
